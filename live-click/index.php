eccf044bba2c0f2fc5c9907319a7267ede320150
macsnoeren <macsnoeren@gmail.com>
2026-05-22 07:59:46 +0200

    First attempt

--- live-click/index.php
+++ live-click/index.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__ . '/includes/auth.php';
 sessionStart();
-header('Location: ' . (currentUser() ? '/dashboard.php' : '/login.php'));
+header('Location: ' . (currentUser() ? 'dashboard.php' : 'login.php'));
 exit;
